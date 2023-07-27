6ee51a7814bfab00998cc224d4e01443c6f649d9
MansoorCM <manzoorcm96@gmail.com>
2023-07-27 14:56:49 +0530

    Added contacts fragment

---
+++ app/src/main/java/com/example/contactsotp/ContactFragment.kt
@@ -0,0 +1,22 @@
+package com.example.contactsotp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.contactsotp.databinding.FragmentContactBinding
+
+class ContactFragment : Fragment() {
+
+    private lateinit var binding: FragmentContactBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentContactBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
